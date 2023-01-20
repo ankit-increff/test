ea4391592df607ab994b74957781093e8908f6f8
ankitkumar <ankit.kumar@increff.com>
2023-01-20 12:02:10 +0530

    signup added

---
+++ src/main/java/com/increff/employee/model/SignupForm.java
@@ -0,0 +1,14 @@
+package com.increff.employee.model;
+
+public class SignupForm extends LoginForm{
+
+	private String confirmPassword;
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+}
